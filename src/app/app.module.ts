1c4df169a8fd34bafee821619096a785b37444f6
CarlosEngineer <co9852587@gmail.com.br>
2024-10-19 13:12:31 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConteinerComponent } from './componentes/conteiner/conteiner.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConteinerComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule
